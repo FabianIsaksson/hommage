76f6215da5ed49b7a7c73c2f5a4e60a0ebe53a52
Fabian Isaksson <fabi.isak@gmail.com>
2022-01-16 23:31:13 +0100

    preventing scroll

--- src/hooks/useDisableUserScroll.ts
+++ src/hooks/useDisableUserScroll.ts
@@ -1,6 +1,15 @@
 import { useEffect } from "react";
 
 export const useDisableUserScroll = () => {
+  document.body.addEventListener(
+    "touchmove",
+    function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    },
+    false,
+  );
+
   useEffect(() => {
     document.body.classList.add("disable-touch-scroll");
 

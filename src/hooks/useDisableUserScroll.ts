0a12ed2f285f89f733938ddbf90113e27d2f7f2b
Fabian Isaksson <fabi.isak@gmail.com>
2022-01-17 01:08:38 +0100

    scrollable animation

--- src/hooks/useDisableUserScroll.ts
+++ src/hooks/useDisableUserScroll.ts
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export const useDisableUserScroll = () => {
   useEffect(() => {
-    document.body.classList.add("disable-touch-scroll");
+    // document.body.classList.add("disable-touch-scroll");
 
     return () => {
       document.body.classList.remove("disable-touch-scroll");

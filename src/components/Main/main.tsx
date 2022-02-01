075244edb73fea1e101ae9eb8f5999ea37b84e47
Fabian Isaksson <fabi.isak@gmail.com>
2022-02-01 14:58:22 +0100

    fix

--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -73,7 +73,7 @@ const Main = () => {
           <Player options={videoJsOptions} onReady={handlePlayerReady} />
         </div>
 
-        <div className="main-content" style={{ opacity: showFrame ? 0 : 1 }}>
+        <div className="main-content">
           <Logo className="main-logo" style={{ opacity: logoFade }} />
           <p
             onClick={() => {
